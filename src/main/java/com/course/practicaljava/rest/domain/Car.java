202eb1cc5f67de656cc00465add20749d9ece5be
Teddy Wijaya <teddy.wijaya@bfi.co.id>
2019-01-04 10:57:05 +0700

    Commit Car.java & CarRestController.java
    add setter getter for new attribute and create echo method

--- src/main/java/com/course/practicaljava/rest/domain/Car.java
+++ src/main/java/com/course/practicaljava/rest/domain/Car.java
@@ -1,32 +1,66 @@
 package com.course.practicaljava.rest.domain;
 
+import java.util.Date;
+
 public class Car {
 	
 	private String brand;
 	private String color;
 	private String type;
+	private int price;
+	private boolean available;
+	private Date firstReleaseDate;
 	
+	//constructor untuk POST Json
+	public Car() {
+		super();
+	}
+
 	public Car(String brand, String color, String type) {
 		super();
 		this.brand = brand;
 		this.color = color;
 		this.type = type;
 	}
-	
+
 	public String getBrand() {
 		return brand;
 	}
+
 	public String getColor() {
 		return color;
 	}
+
+	public Date getFirstReleaseDate() {
+		return firstReleaseDate;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
 	public String getType() {
 		return type;
+	}
+	
+	public boolean isAvailable() {
+		return available;
+	}
+	
+	public void setAvailable(boolean available) {
+		this.available = available;
 	}
 	public void setBrand(String brand) {
 		this.brand = brand;
 	}
 	public void setColor(String color) {
 		this.color = color;
+	}
+	public void setFirstReleaseDate(Date firstReleaseDate) {
+		this.firstReleaseDate = firstReleaseDate;
+	}
+	public void setPrice(int price) {
+		this.price = price;
 	}
 	
 	public void setType(String type) {
